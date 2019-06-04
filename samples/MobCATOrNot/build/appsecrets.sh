4bfb7165588d13f8e06a974594ef86714fb5c0b1
Sweeky Satpathy <swsat@microsoft.com>
2019-06-04 15:32:55 -0700

    release yml updated, version task added to build step, app secrets updated for constants check

--- samples/MobCATOrNot/build/appsecrets.sh
+++ samples/MobCATOrNot/build/appsecrets.sh
@@ -15,17 +15,17 @@ then
     exit
 fi
 
-if [ ! -n $(AzureFunctionsUrl) ]
-then
-    echo "You need define the AzureFunctionsUrl variable in VSTS"
-    exit
-fi
+# if [ ! -n $(AzureFunctionsUrl) ]
+# then
+#     echo "You need define the AzureFunctionsUrl variable in VSTS"
+#     exit
+# fi
 
-if [ ! -n $(AzureFunctionsCode) ]
-then
-    echo "You need define the AzureFunctionsCode variable in VSTS"
-    exit
-fi
+# if [ ! -n $(AzureFunctionsCode) ]
+# then
+#     echo "You need define the AzureFunctionsCode variable in VSTS"
+#     exit
+# fi
 
 APP_CONSTANT_FILE=samples/MobCATOrNot/MobCATOrNot/Constants.cs
 
@@ -37,11 +37,11 @@ then
  echo "Updating App Secret Values to CustomVisionTrainingKey in Constants.cs"
     sed -i '' 's#CustomVisionTrainingKey = "[a-z:./\\_]*"#CustomVisionTrainingKey = "$(CustomVisionTrainingKey)"#' $APP_CONSTANT_FILE
 
- echo "Updating App Secret Values to AzureFunctionsUrl in Constants.cs"
-    sed -i '' 's#AzureFunctionsUrl = "[a-z:./\\_]*"#AzureFunctionsUrl = "$(AzureFunctionsUrl)"#' $APP_CONSTANT_FILE
+#  echo "Updating App Secret Values to AzureFunctionsUrl in Constants.cs"
+#     sed -i '' 's#AzureFunctionsUrl = "[a-z:./\\_]*"#AzureFunctionsUrl = "$(AzureFunctionsUrl)"#' $APP_CONSTANT_FILE
 
- echo "Updating App Secret Values to AzureFunctionsCode in Constants.cs"
-    sed -i '' 's#AzureFunctionsCode = "[a-z:./\\_]*"#AzureFunctionsCode = "$(AzureFunctionsCode)"#' $APP_CONSTANT_FILE
+#  echo "Updating App Secret Values to AzureFunctionsCode in Constants.cs"
+#     sed -i '' 's#AzureFunctionsCode = "[a-z:./\\_]*"#AzureFunctionsCode = "$(AzureFunctionsCode)"#' $APP_CONSTANT_FILE
 
     echo "File content:"
     cat $APP_CONSTANT_FILE
